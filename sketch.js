315bf2b9e66cab839dd2f67752631197529c20df
unknown <baby2pooja@gmail.com>
2021-08-10 14:44:42 +0530

    platform added

--- sketch.js
+++ sketch.js
@@ -14,10 +14,11 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
+    
     
     ground = new Ground(600,height,1200,20)
-
+    platform=new Ground(150,300,300,170)
+    
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -59,4 +60,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
